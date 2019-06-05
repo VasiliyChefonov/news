73e657afc9e008508a81509ad6ef947cee4137fb
Kub <chefonov99@mail.ru>
2019-06-05 14:24:30 +0700

    release

--- thenews/settings.py
+++ thenews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eyik9(j7gkm&^%-x7lep&(cgith0tzg)h*eca6-r(m1p+4o4ow'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
